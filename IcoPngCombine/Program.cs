f5deeec69cfff78012d4c247003276babe3d4832
Slava Kolobaev <gmanyy@gmail.com>
2021-03-12 17:11:56 +0100

    Initial commit.

---
+++ IcoPngCombine/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.IconLib;
+using System.Drawing.IconLib.Exceptions;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace IcoPngCombine
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: IcoPngCombine.exe [start_folder]");
+                Console.WriteLine("Start folder should contain this directory/file structure:");
+                Console.WriteLine("    size\\category\\icon_name.png");
+                Console.WriteLine("Like you can see in this repo: https://github.com/KDE/oxygen-icons");
+                Console.WriteLine("This tool creates a \"results\" folder in the start folder and puts all of the combined icons there");
+
+                return;
+            }
+
+            string startFolder = args[0];
+            if (!Directory.Exists(startFolder))
+            {
+                Console.Error.WriteLine($"Directory {startFolder} doesn't exist.");
+
+                return;
+            }
+
+            string results = Path.Combine(startFolder, "results");
+            Directory.CreateDirectory(results);
+
+            string[] sizes = Directory.GetDirectories(startFolder);
+
+            HashSet<string> createdIcons = new HashSet<string>();
+
+            foreach (string scannedSize in sizes)
+            {
+                string[] categories = Directory.GetDirectories(scannedSize);
+                foreach (string category in categories)
+                {
+                    string catName = Path.GetFileName(category);
+
+                    string[] icons = Directory.GetFiles(category, "*.png");
+                    foreach (string icon in icons)
+                    {
+                        string iconFileName = Path.GetFileName(icon);
+                        string iconName = Path.GetFileNameWithoutExtension(icon);
+
+                        string iconFullPath = Path.Combine(catName, iconFileName);
+                        string iconId = Path.Combine(catName, iconName);
+
+                        if (createdIcons.Contains(iconId))
+                        {
+                            continue;
+                        }
+
+                        createdIcons.Add(iconId);
+
+                        MultiIcon img = new MultiIcon();
+                        int index = -1, selectedIndex = -1, selectedIndexSize = 0;
+                        List<string> szs = new List<string>();
+                        foreach (string sz in sizes)
+                        {
+                            string fullPath = Path.Combine(sz, iconFullPath);
+                            if (!File.Exists(fullPath))
+                            {
+                                continue;
+                            }
+
+                            string sizeName = Path.GetFileName(sz);
+                            
+                            SingleIcon ic = img.Add(sizeName);
+
+                            try
+                            {
+                                Bitmap bmp = (Bitmap) Bitmap.FromFile(fullPath);
+
+                                try
+                                {
+                                    ic.CreateFrom(bmp, IconOutputFormat.Vista);
+
+                                    szs.Add(sizeName);
+                                    
+                                    index += 1;
+                                    if (!Int32.TryParse(new String(sizeName.TakeWhile(c => c >= '0' && c <= '9').ToArray()), out int size))
+                                    {
+                                        size = 0;
+                                    }
+
+                                    if (selectedIndex < 0 || selectedIndexSize < size)
+                                    {
+                                        selectedIndex = index;
+                                        selectedIndexSize = size;
+                                    }
+                                }
+                                catch (ImageTooBigException)
+                                {
+                                    Console.WriteLine($"Image {sizeName}\\{iconId} has dimensions that are too big: w={bmp.Width}, h={bmp.Height}");
+                                
+                                    img.Remove(ic);
+                                }
+                            }
+                            catch (InvalidPixelFormatException ex)
+                            {
+                                Console.WriteLine($"Ignoring size {sizeName} of {iconId} because {ex.Message}, required is {PixelFormat.Format32bppArgb}");
+                                
+                                img.Remove(ic);
+                            }
+                        }
+
+                        if (selectedIndex < 0)
+                        {
+                            continue;
+                        }
+
+                        string dirName = Path.Combine(results, catName);
+                        if (!Directory.Exists(dirName))
+                        {
+                            Directory.CreateDirectory(dirName);
+                        }
+
+                        img.SelectedIndex = selectedIndex;
+
+                        Console.WriteLine($"Saving {iconId} with sizes [{String.Join(", ", szs)}]");
+
+                        string icoName = Path.Combine(results, iconId) + ".ico";
+                        img.Save(icoName, MultiIconFormat.ICO);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Done. Created {createdIcons.Count} icons.");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # IcoPngCombine
-A small tool that combines an icon set represented as a set of PNGs grouped by sizes/types/icons into a set of .ICO files.
+A small tool that combines an icon set represented as a set of PNGs into a set of .ICO files.
+
+# Building
+As this is not intended to be a packaged solution, you'll need to build this tool yourself.
+
+To do this
+
+1. Clone this repo with submodules.
+2. Fix a typo in a `using` directive at the top of the file `IcoPngCombine\IconLib\System\Drawing\IconLib\LibraryFormats\PEFormat.cs`, it should say `IconLib` instead of `IconLIb`. This is a typo in the submodule, so I'm not able to incorporate a fix here.
+3. Build the `IcoPngCombine\IcoPngCombine.csproj` normally with .NET Core SDK or Visual Studio.
+
+# Usage
+This tool understands PNG files organized in the `size\category\icon_name.png` file structure. And example of this structure can be found in [KDE's Oxygen Icons](https://github.com/KDE/oxygen-icons) repo.
+
+Supply the path to the directory containing size folders as a first argument to the tool and it will create a `results` folder inside the supplied folder and put all icons, organized by categories, into it.
+
+The tool was only tested on Windows and its operation on other OSes is not guaranteed since it uses some parts of Windows Forms library. Although, nothing graphical, so it may work.
